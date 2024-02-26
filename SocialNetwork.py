c0c03682c64354f2b108b879b1f4f3e88d0f80bc
DanielS4495 <daniel.samson@msmail.ariel.ac.il>
2024-02-26 16:32:28 +0200

    oop2

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,6 +1,5 @@
 # Singleton Design Pattern
 from datetime import datetime
-
 import matplotlib.pyplot as plt
 class Singleton(type):
     _instances = {}
@@ -13,38 +12,27 @@ class Singleton(type):
 
 # Factory Design Pattern
 class PostFactory:
-    def create_post(self, post_type, post_id, content, created_at, created_by, **kwargs):
-        if post_type == 'Text':
-            print(f"{created_by.username} published a post:\n\"{content}\"\n")
-            return TextPost(post_id, content, created_at, created_by)
-        elif post_type == 'Image':
-            print(f"{created_by.username} posted a picture\n")
-            return ImagePost(post_id, content, created_at, created_by, kwargs['image_path'])
-        elif post_type == 'Sale':
-            print(f"{created_by.username} posted a product for sale:\nFor sale! {kwargs['description']}, price: {kwargs['price']}, pickup from: {kwargs['location']}\n")
-            return SalePost(post_id, content, created_at, created_by, kwargs['description'], kwargs['price'], kwargs['location'])
-        else:
-            raise ValueError("Invalid post type")
+    def create_post(self, post_type, **kwargs):
+        try:
+            if post_type == 'Text':
+                print(f"{kwargs['created_by']} published a post:\n\"{kwargs['content']}\"\n")
+                return TextPost(kwargs['post_id'], kwargs['content'], kwargs['created_at'], kwargs['created_by'])
+            elif post_type == 'Image':
+                print(f"{kwargs['created_by']} posted a picture\n")
+                return ImagePost(kwargs['post_id'], kwargs['content'], kwargs['created_at'], kwargs['created_by'], kwargs['image_path'])
+            elif post_type == 'Sale':
+                print(f"{kwargs['created_by']} posted a product for sale:\nFor sale! {kwargs['description']}, price: {kwargs['price']}, pickup from: {kwargs['location']}\n")
+                return SalePost(kwargs['post_id'], kwargs['content'], kwargs['created_at'], kwargs['created_by'], kwargs['description'], kwargs['price'], kwargs['location'])
+            else:
+                raise ValueError("Invalid post type")
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
 
 class Observer:
     def update(self, message):
         pass
-
-
-# class Subject:
-#     def __init__(self):
-#         self._observers = set()
-#
-#     def add_observer(self, observer):
-#         self._observers.add(observer)
-#
-#     def remove_observer(self, observer):
-#         self._observers.remove(observer)
-#
-#     def notify_observers(self, message):
-#         for observer in self._observers:
-#             observer.update(message)
 
 from datetime import datetime
 class User(Observer):
@@ -60,7 +48,6 @@ class User(Observer):
         self.posts = []
         self.notifications = []
         self.social_network = social_network
-       # social_network.add_observer(self)
 
     def create_post(self, content):
         post = Post(len(self.posts) + 1, content, datetime.now(), self)
@@ -71,29 +58,27 @@ class User(Observer):
 
     def like_post(self, post):
         if post.created_by != self:
-            # post.add_like(self)
             print(f"notification to {post.created_by.username}: {self.username} liked your post")
             post.created_by.add_notification(f"{self.username} liked your post")
 
     def comment_on_post(self, post, comment):
-        # post.add_comment(self, comment)
         if post.created_by.username != self:
             print(f"notification to {post.created_by.username}: {self.username} commented on your post: {comment}")
             post.created_by.add_notification(f"{self.username} commented on your post")
 
     def follow(self, other_user):
-        if other_user != self:
-            other_user.followers.add(self)
-            # self.following.add(other_user)
-            print(f"{self.username} started following {other_user.username}")
-        else:
-            print("You can't follow yourself.")
-        # for post in other_user.posts:
-        #     self.notify(other_user, f"{self.username} started following you and you have a new post", "follow_post")
+        try:
+            if other_user != self:
+                other_user.followers.add(self)
+                print(f"{self.username} started following {other_user.username}")
+            else:
+                raise (Exception("You can't follow yourself."))
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def unfollow(self, other_user):
         other_user.followers.remove(self)
-        # self.following.remove(other_user)
         print(f"{self.username} unfollowed {other_user.username}")
 
     def notify(self, message):
@@ -102,86 +87,29 @@ class User(Observer):
 
     def add_notification(self,message):
         self.notifications.append(message)
-
-    # need to change!!!!!!!!!!!!!!!!!
-    # def print_notifications(self):
-    #     print(f"{self.username}'s notifications:")
-    #     for notification in self.notifications:
-    #         print(notification)
-    #     for post in self.posts:
-    #         for like in post.likes:
-    #             if like != self:
-    #                 print(f"{like.username} liked your post")
-    #         for comment in post.comments:
-    #             if comment[0] != self:
-    #                 print(f"{comment[0].username} commented on your post")
-    #     for follower in self.followers:
-    #         for follower_post in follower.posts:
-    #             if follower_post not in self.posts:
-    #                 print(f"{follower.username} has a new post")
-    #             for follower_like in follower_post.likes:
-    #                 if follower_like == self:
-    #                     print(f"{follower.username} liked {follower_post.created_by.username}'s post.")
-    #             for follower_comment in follower_post.comments:
-    #                 if follower_comment[0] == self:
-    #                     print(f"{follower.username} commented on {follower_post.created_by.username}'s post: {follower_comment[1]}")
-
-# /////////////
-    # def print_notifications(self):
-    #     print(f"{self.username}'s notifications:")
-        
-    #     # Collect notifications
-    #     notifications = []
-
-    #     for post in self.posts:
-    #         for like in post.likes:
-    #             if like != self:
-    #                 notifications.append((like.username, f"{like.username} liked your post"))
-    #         for comment in post.comments:
-    #             if comment[0] != self:
-    #                 notifications.append((comment[0].username, f"{comment[0].username} commented on your post"))
-
-    #     for follower in self.followers:
-    #         for follower.posts in follower.posts:
-    #             if follower.posts not in self.posts:
-    #                 notifications.append((follower.username, f"{follower.username} has a new post"))
-    #             for follower_like in follower.posts.likes:
-    #                 if follower_like == self:
-    #                     notifications.append((follower.username, f"{follower.username} liked {follower.posts.created_by.username}'s post."))
-    #             for follower_comment in follower.posts.comments:
-    #                 if follower_comment[0] == self:
-    #                     notifications.append((follower.username, f"{follower.username} commented on {follower.posts.created_by.username}'s post: {follower_comment[1]}"))
-
-    #     # Print notifications in the order they were added
-    #     for _, notification in notifications:
-    #         print(notification)
-        
+   
     def print_notifications(self):
         print(f"{self.username}'s notifications:")
         for notification in self.notifications:
             print(notification)
 
-
-
     def publish_post(self, post_type, ConOrPath, price=None, location=None):
-        if not self.logged_in:
-            raise PermissionError("User not logged in")
-        post_id = len(self.posts) + 1
-        created_at = datetime.now()
-        post_factory = PostFactory()
-        if post_type == 'Text':
-            new_post = post_factory.create_post(post_type, post_id, ConOrPath, created_at, self)
-        elif post_type == 'Image':
-            new_post = post_factory.create_post(post_type, post_id,content=None,created_at=created_at, created_by=self,image_path=ConOrPath)
-        elif post_type == 'Sale':
-          new_post = post_factory.create_post(post_type, post_id, content=None, created_at=created_at, created_by=self,image_path=None,description=ConOrPath,price=price,location=location)
-        else:
-            raise ValueError("Invalid post type")
-        self.posts.append(new_post)
-        for user in self.followers:
-            if self.username != user:
-                user.add_notification(f"{self.username} has a new post")
-        return new_post
+        try:
+            if not self.logged_in:
+                raise PermissionError("User not logged in")
+            else:
+                post_id = len(self.posts) + 1
+                created_at = datetime.now()
+                post_factory = PostFactory()
+                new_post = post_factory.create_post(post_type,post_id=post_id,content=ConOrPath,image_path=ConOrPath,created_at=created_at,created_by=self,description=ConOrPath,price=price,location=location)
+                self.posts.append(new_post)
+                for user in self.followers:
+                    if self.username != user:
+                        user.add_notification(f"{self.username} has a new post")
+                return new_post
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def __str__(self):
         return f"User name: {self.username}, Number of posts: {len(self.posts)}, Number of followers: {len(self.followers)}"
@@ -193,18 +121,17 @@ class Post:
         self.content = content
         self.created_at = created_at
         self.created_by = created_by
-        self.likes = set()
+        self.likes = []
         self.comments = []
 
     def like(self, user):
-        self.likes.add(user)
+        self.likes.append(user)
         user.like_post(self)
 
     def comment(self, user, comment):
         self.comments.append((user, comment))
         user.comment_on_post(self,comment)
 
-    # need to do a picture
     def display(self):
         pass
 
@@ -215,8 +142,6 @@ class Post:
             return f"{self.created_by.username} posted a picture\n"
         elif isinstance(self, SalePost):
             return f"{self.created_by.username} published a sale post:\n{self.description}, price: {self.price}, pickup from: {self.location}"
-        else:
-            return "Unknown post type"
 
 class TextPost(Post):
     pass
@@ -230,14 +155,15 @@ class ImagePost(Post):
         self.image_path = image_path
 
     def display(self):
-        print(f"Shows picture")
-        img = mpimg.imread(self.image_path)
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
-
-   
-
+        try:
+            print(f"Shows picture")
+            img = mpimg.imread(self.image_path)
+            plt.imshow(img)
+            plt.axis('off')
+            plt.show()
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
 class SalePost(Post):
     def __init__(self, post_id, content, created_at, created_by, description, price, location, available=True):
@@ -248,26 +174,33 @@ class SalePost(Post):
         self.available = available
 
     def discount(self, percent, password):
-        if self.created_by.password == password:
-            if  isinstance(percent, int):
-                self.price -= (self.price * percent) / 100
-                print(f"Discount on {self.created_by.username} product! the new price is: {self.price}")
-        else:
-            raise PermissionError("Incorrect password for discount")
+        try:
+            if self.created_by.password == password:
+                if  isinstance(percent, int):
+                    self.price -= (self.price * percent) / 100
+                    print(f"Discount on {self.created_by.username} product! the new price is: {self.price}")
+            else:
+                raise PermissionError("Incorrect password for discount")
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def sold(self, password):
-        if self.created_by.password == password:
-            self.available = False
-            print(f"{self.created_by.username}'s product is sold")
-        else:
-            raise PermissionError("Incorrect password for marking as sold")
+        try:
+            if self.created_by.password == password:
+                self.available = False
+                print(f"{self.created_by.username}'s product is sold")
+            else:
+                raise PermissionError("Incorrect password for marking as sold")
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def __str__(self):
         status = "Available" if self.available else "Sold"
         return f"{self.created_by.username} posted a product for sale:\n{status}! {self.description}, price: {self.price}, pickup from: {self.location}\n"
 
 class SocialNetwork(metaclass=Singleton):
-    # צריך לאכוף את זה שלא נוצר עוד אחד
     def __init__(self, name):
         super().__init__()
         self.name = name
@@ -275,17 +208,16 @@ class SocialNetwork(metaclass=Singleton):
         print(f"The social network {name} was created!")
 
     def sign_up(self, username, password):
-        # Check if the username is already taken
-        if any(user.username == username for user in self.users):
-            print(f"The username '{username}' is already taken.")
+        try: 
+            # Check if the username is already taken
+            if any(user.username == username for user in self.users):
+                raise (Exception(f"The username '{username}' is already taken."))
+            # Check password length
+            if not (4 <= len(password) <= 8):
+                raise (Exception("Password length should be between 4 and 8 characters.")) 
+        except Exception as e:
+            print("An error occurred:", e)
             return None
-
-        # Check password length
-        if not (4 <= len(password) <= 8):
-            print("Password length should be between 4 and 8 characters.")
-            return None
-
-        # Create a new user and add to the network
         user_id = len(self.users) + 1
         new_user = User(user_id, username, password, self)
         self.users.append(new_user)
@@ -293,34 +225,50 @@ class SocialNetwork(metaclass=Singleton):
         return new_user
 
     def log_in(self, username, password):
-        for user in self.users:
-            if user.username == username:
-                if user.password == password:
-                    if user.logged_in==True:
-                        print(f"already connected")
+        try:
+            for user in self.users:
+                if user.username == username:
+                    if user.password == password:
+                        if user.logged_in==True:
+                            raise (Exception(f"already connected"))
+                            return False
+                        else:
+                            user.logged_in = True
+                            print(f"{username} connected")
+                            return True
                     else:
-                        user.logged_in = True
-                        print(f"{username} connected")
-                        return True
-                else:
-                    print("Incorrect password")
-                    return False
-        print("User not found")
-        return None
+                        raise (Exception("Incorrect password"))
+                        return False
+            raise (Exception("User not found"))
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def log_out(self, username):
-        for user in self.users:
-            if user.username==username:
-                user.logged_in = False
-                print(f"{user.username} disconnected")
-                return None
-        print("User not found")
+        try:
+            for user in self.users:
+                if user.username == username:
+                    if user.logged_in == True:
+                        user.logged_in = False
+                        print(f"{user.username} disconnected")
+                        return None
+                    else:
+                        raise (Exception("User already disconnected"))
+                        return None
+            raise (Exception("User not found"))
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def get_user(self, username):
-        for user in self.users:
-            if user.username == username:
-                return user
-        return None
+        try:
+            for user in self.users:
+                if user.username == username:
+                    return user
+            raise (Exception("User not found"))
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
 
     def __str__(self):
         result = "Twitter social network:\n"
